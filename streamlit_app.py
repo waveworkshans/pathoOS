8f0386bd0efd0fb09a6ccb995803c74833f4e3b0
waveworkshans <149389079+waveworkshans@users.noreply.github.com>
2023-11-23 06:29:18 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -140,7 +140,7 @@ def main():
     st.title('Data Reformatter')
     
     # Sample input data
-    input_data = st.text_area("Insert input:", "Paste your data here...")
+    input_data = st.text_area("Insert Pathology input:", "Paste your data here...")
     
     if input_data and "Paste your data here..." not in input_data:
         dates, output_data = reformat_data(input_data)
@@ -165,32 +165,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-# Replace your main function with this
-#def main():
-    st.title('Data Reformatter')
-    # Sample input data
-    input_data = st.text_area("Pathology input:", "Paste your data here...")
-    
-    # Check if input_data is not the placeholder
-    if input_data and "Paste your data here..." not in input_data:
-        # Reformat the data
-        dates, output_data = reformat_data(input_data)
-        if len(output_data) < 1:
-            st.write("No data to display.")
-            return
-        results = ''
-        for i, date in enumerate(dates):
-            output_data_sub = ''
-            for x in output_data:
-                if x[1][i] != '':
-                    output_data_sub += f"{x[0]}{x[1][i]}/"
-            output_data_sub = f"({date})\n{output_data_sub[:-1]}\n"
-            results += output_data_sub
-        st.text('Output:')
-        st.write(results)
-    else:
-        st.write("Please paste your data in the text area above.")
 
-# Use Streamlit's way of running the app
-if __name__ == "__main__":
-    main()
